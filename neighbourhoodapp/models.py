90543706cd5a420a1277f76063a0ad6fbad4ddda
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-25 18:42:23 +0300

    created class for user

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighbourhood(models.Model):
-    name=models.CharField(max_lenght=50)
-    location=models.CharField(max_lenght=60)
+    name=models.CharField(max_length=50)
+    location=models.CharField(max_length=60)
     admin=models.ForeignKey("Profile",on_delete=models.CASCADE,related_name='hood')
     occupants_count=models.IntegerField(null=True,blank=True)
     description=models.TextField()
@@ -21,3 +21,15 @@ class Neighbourhood(models.Model):
     @classmethod
     def find_neighborhood(cls,neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name=models.CharField(blank=True,max_length=30)
+    email=models.CharField(blank=True,max_length=100)
+    status=models.TextField(max_length=100)
+    profile_image=models.ImageField(upload_to='images/')
+    location=models.CharField(max_length=50,blank=True,null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
