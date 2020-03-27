f665158d3ab6a0c2c58b497ab659f180c3705079
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-27 17:42:46 +0300

    corrected on indentation

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -8,9 +8,11 @@ from django.dispatch import receiver
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+    
 @receiver(post_save, sender=User)
 def create_hood_admin(sender, instance, created, **kwargs):
     if created:
