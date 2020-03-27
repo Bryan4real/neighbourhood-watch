f8517cbd62f5023637602028cf9f9b2d19c4b68d
ian mwangi <iankimotho233@gmail.com>
2020-03-27 16:48:53 +0300

    Add the profile, business and post serializers

--- neighbourhoodapp/serializer.py
+++ neighbourhoodapp/serializer.py
@@ -8,3 +8,20 @@ class NeighbourhoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Neighbourhood
         fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class BusinessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Business
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
