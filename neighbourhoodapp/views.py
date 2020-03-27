a89232ead28b43e4ddacda6f672cd79e80b13b94
ian mwangi <iankimotho233@gmail.com>
2020-03-27 16:55:59 +0300

    Create the API view for admin to delete user, to create user profile and admin to create neighbourhoods

--- neighbourhoodapp/views.py
+++ neighbourhoodapp/views.py
@@ -32,3 +32,55 @@ class LoginView(APIView):
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class DeleteUser(generics.DestroyAPIView):
+    def destroy(self, request, *args, **kwargs):
+        system_admin = SystemAdmin.objects.get(pk=self.kwargs["id"])
+        if system_admin.is_admin:
+            try:
+                queryset = User.objects.get(pk=self.kwargs["pk"])
+            except ObjectDoesNotExist:
+                return Response({"User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+            queryset.delete()
+            return Response({"User deleted"})
+
+        else:
+            return Response({"Not authorized"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfile(generics.RetrieveUpdateAPIView):
+    def get_queryset(self):
+        queryset = Profile.objects.filter(user_id=self.kwargs["pk"])
+        return queryset
+
+    serializer_class = ProfileSerializer
+
+    def put(self, request, *args, **kwargs):
+        profile = Profile.objects.get(pk=self.kwargs["pk"])
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateNeighbourhood(generics.ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        system_admin = SystemAdmin.objects.get(pk=self.kwargs["pk"])
+        if system_admin.is_admin:
+            name = request.data.get("name")
+            location = request.data.get("location")
+            occupants_count = request.data.get("occupants_count")
+            data = {"name": name, "location": location, "occupants_count": occupants_count}
+            serializer = NeighbourhoodSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        else:
+            return Response({"Not authorized"}, status=status.HTTP_400_BAD_REQUEST)
