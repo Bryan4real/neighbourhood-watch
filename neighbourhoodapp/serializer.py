8728435ab2dbec33bd703029f83c1a1065062d9d
ian mwangi <iankimotho233@gmail.com>
2020-03-27 16:51:19 +0300

    Add the user serializer that generates a token for every new user

--- neighbourhoodapp/serializer.py
+++ neighbourhoodapp/serializer.py
@@ -43,3 +43,21 @@ class SystemAdminSerializer(serializers.ModelSerializer):
     class Meta:
         model = SystemAdmin
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
